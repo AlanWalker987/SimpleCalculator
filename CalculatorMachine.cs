3af1bd1df2af733c949f3ab5d2f6dfc43d5e3acc
DELL7470-ALG\DELL-UFI <karthik.g@cohesionib.com>
2022-08-17 12:23:47 +0530

    Add project files.

---
+++ CalculatorMachine.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    internal class CalculatorMachine
+    {
+        private double firstNumber;
+        private double secondNumber;
+        private double result;
+        private char operation;
+
+        public CalculatorMachine(double first, double second, char op)
+        {
+            firstNumber = first;
+            secondNumber = second;
+            operation = op;
+            switching();
+        }
+
+        private void switching()
+        {
+            switch (operation)
+            {
+                case '+':
+                    result = firstNumber + secondNumber;
+                    Console.WriteLine($"Addition of {firstNumber} and {secondNumber} is {result}");
+                    startover();
+                    break;
+                case '-':
+                    result = firstNumber - secondNumber;
+                    Console.WriteLine($"Difference of {firstNumber} and {secondNumber} is {result}");
+                    startover();
+                    break;
+                case '*':
+                    result = firstNumber * secondNumber;
+                    Console.WriteLine($"Addition of {firstNumber} and {secondNumber} is {result}");
+                    startover(); 
+                    break;
+                case '/':
+                    result = firstNumber / secondNumber;
+                    Console.WriteLine($"Addition of {firstNumber} and {secondNumber} is {result}");
+                    startover();
+                    break;
+                default: Console.WriteLine("Invalid Operator. Please enter the valid operator");
+                    end();
+                    break;
+            }
+        }
+
+        private void startover()
+        {
+            Console.WriteLine("You want to continue ? Press Y to continue else press any key to exit");
+            char YesorNo = char.Parse(Console.ReadLine());
+            bool response = (YesorNo == 'Y' || YesorNo == 'y');
+
+            if (response)
+            {
+                repeat();
+            }
+            else
+            {
+                end();
+            }
+
+        }
+
+        private void repeat()
+        {
+            firstNumber = result;
+
+            Console.WriteLine("Enter the second number");
+            secondNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Choose the Operation");
+            operation = char.Parse(Console.ReadLine());
+
+            switching();
+        }
+
+        private void end()
+        {
+            Console.WriteLine($"Final Result is {result}");
+            Console.WriteLine("Press any key to exit the calculator");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("------------Calculator Operations-------------------");
+
+            Console.WriteLine("Enter First Number");
+            double firstnum = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number");
+            double secondNum = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Choose the operation you want to perform - (+, -, *, /)");
+            char op = char.Parse(Console.ReadLine());
+
+            CalculatorMachine cm = new CalculatorMachine(firstnum, secondNum, op);
+            Console.ReadKey();
+        }
+    }
+
+}
